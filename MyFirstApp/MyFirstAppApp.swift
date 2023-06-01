df3807bf0b1249e4a1c57ae4126d9e5bf3208ce8
c-owner <21428025@stu.yd.ac.kr>
2023-06-01 17:06:25 +0900

    test1

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by corner on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
