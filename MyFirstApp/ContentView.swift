02d3c31023b0724309161e0a18f1f8ec2e4a4126
c-owner <21428025@stu.yd.ac.kr>
2023-06-09 13:43:41 +0900

    project init

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  MyFirstApp
 //
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, corner!")
         }
         .padding()
     }
